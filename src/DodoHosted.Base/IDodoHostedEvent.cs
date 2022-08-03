309e18908954ea8c7fe01d80afb158e5f3490d60
Liam Sho <conan_creeper@outlook.com>
2022-08-03 18:43:58 +0800

    feat: 添加 Events

---
+++ src/DodoHosted.Base/IDodoHostedEvent.cs
@@ -0,0 +1,18 @@
+// This file is a part of Dodo.Hosted project.
+// 
+// Copyright (C) 2022 LiamSho and all Contributors
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY
+
+namespace DodoHosted.Base;
+
+/// <summary>
+/// 标记 Dodo.Hosted 封装过的 Events
+/// </summary>
+public interface IDodoHostedEvent { }
